f14d7efbdebd42cfc31fa681f910a4e3853a05bf
UR0609 <124732719@qq.com>
2020-02-13 09:33:47 +0800

    springcloud eureka

---
+++ eureka/src/test/java/com/ljryh/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljryh.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
