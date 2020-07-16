7580114c9ff821d58b5981403c63b60579ea25d4
UR0609 <124732719@qq.com>
2020-07-16 08:53:28 +0800

    redis乱码

---
+++ client/src/main/java/com/ljryh/client/config/redis/RedisTemplateConfig.java
@@ -0,0 +1,26 @@
+package com.ljryh.client.config.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class RedisTemplateConfig {
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Bean
+    public RedisTemplate redisTemplateInit() {
+        //设置序列化Key的实例化对象
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置序列化Value的实例化对象
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+
+}
