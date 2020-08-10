fee57c3264640d97b7a1c376f1042150fd557b93
UR0609 <124732719@qq.com>
2020-08-10 15:49:01 +0800

    ribbon

--- ribbon/src/main/java/com/ljryh/ribbon/controler/RibbonController.java
+++ ribbon/src/main/java/com/ljryh/ribbon/controler/RibbonController.java
@@ -3,6 +3,7 @@ package com.ljryh.ribbon.controler;
 import com.ljryh.ribbon.service.RibbonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,12 @@ public class RibbonController {
     @Autowired
     private RibbonService ribbonService;
 
-    @RequestMapping(value = "/getRibbon")
+    @RequestMapping(value = "/getRibbon",method = RequestMethod.GET)
     public String getRibbon(@RequestParam String data) {
         return ribbonService.get(data);
     }
 
-    @RequestMapping(value = "/postRibbon")
+    @RequestMapping(value = "/postRibbon",method = RequestMethod.POST)
     public String postRibbon() {
         return ribbonService.post();
     }
