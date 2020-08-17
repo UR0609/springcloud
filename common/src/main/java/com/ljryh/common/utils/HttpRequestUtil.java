e092d6823a0cd0e64973f876fa836165acc1339f
UR0609 <124732719@qq.com>
2020-08-17 10:24:41 +0800

    http 超时时间

--- common/src/main/java/com/ljryh/common/utils/HttpRequestUtil.java
+++ common/src/main/java/com/ljryh/common/utils/HttpRequestUtil.java
@@ -28,6 +28,8 @@ public class HttpRequestUtil {
             connection.setRequestProperty("connection", "Keep-Alive");
             connection.setRequestProperty("user-agent",
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            connection.setConnectTimeout(1000*5);
+            connection.setReadTimeout(1000*5);
             // 建立实际的连接
             connection.connect();
             // 获取所有响应头字段
@@ -82,6 +84,8 @@ public class HttpRequestUtil {
             conn.setRequestProperty("connection", "Keep-Alive");
             conn.setRequestProperty("user-agent",
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            conn.setConnectTimeout(1000*5);
+            conn.setReadTimeout(1000*5);
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
