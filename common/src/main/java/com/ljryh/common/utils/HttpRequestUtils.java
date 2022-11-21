2e515c67f2e4a6a651eb402f5f85091025862e72
易柏忆 <124732719@qq.com>
2022-11-21 14:50:17 +0800

    commit

--- common/src/main/java/com/ljryh/common/utils/HttpRequestUtils.java
+++ common/src/main/java/com/ljryh/common/utils/HttpRequestUtils.java
@@ -15,7 +15,7 @@ public class HttpRequestUtils {
      * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
      * @return URL 所代表远程资源的响应结果
      */
-    public static String sendGet(String url, String param) {
+    public static String sendGet(String url, String param, Map<String, String> header) {
         String result = "";
         BufferedReader in = null;
         try {
@@ -28,8 +28,13 @@ public class HttpRequestUtils {
             conn.setRequestProperty("connection", "Keep-Alive");
             conn.setRequestProperty("user-agent",
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            conn.setConnectTimeout(1000*5);
-            conn.setReadTimeout(1000*5);
+            conn.setConnectTimeout(1000 * 5);
+            conn.setReadTimeout(1000 * 5);
+            if (header != null) {
+                for (Map.Entry<String, String> entry : header.entrySet()) {
+                    conn.setRequestProperty(entry.getKey(), entry.getValue());
+                }
+            }
             // 建立实际的连接
             conn.connect();
             // 获取所有响应头字段
@@ -39,7 +44,7 @@ public class HttpRequestUtils {
 //                System.out.println(key + "--->" + map.get(key));
 //            }
             // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
             String line;
             while ((line = in.readLine()) != null) {
                 result += line;
@@ -83,8 +88,8 @@ public class HttpRequestUtils {
             conn.setRequestProperty("connection", "Keep-Alive");
             conn.setRequestProperty("user-agent",
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            conn.setConnectTimeout(1000*5);
-            conn.setReadTimeout(1000*5);
+            conn.setConnectTimeout(1000 * 5);
+            conn.setReadTimeout(1000 * 5);
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
@@ -95,7 +100,7 @@ public class HttpRequestUtils {
             // flush输出流的缓冲
             out.flush();
             // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
             String line;
             while ((line = in.readLine()) != null) {
                 result += line;
@@ -123,16 +128,17 @@ public class HttpRequestUtils {
 
     /**
      * 从网络Url中下载文件
+     *
      * @param urlStr
      * @param fileName
      * @param savePath
      * @throws IOException
      */
-    public static void  downLoadFromUrl(String urlStr,String fileName,String savePath/*,String toekn*/) throws IOException{
+    public static void downLoadFromUrl(String urlStr, String fileName, String savePath/*,String toekn*/) throws IOException {
         URL url = new URL(urlStr);
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         //设置超时间为3秒
-        conn.setConnectTimeout(10*1000);
+        conn.setConnectTimeout(10 * 1000);
         //防止屏蔽程序抓取而返回403错误
         conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
 
@@ -145,37 +151,37 @@ public class HttpRequestUtils {
 
         //文件保存位置
         File saveDir = new File(savePath);
-        if(!saveDir.exists()){
+        if (!saveDir.exists()) {
             saveDir.mkdir();
         }
-        File file = new File(saveDir+ File.separator+fileName);
+        File file = new File(saveDir + File.separator + fileName);
         FileOutputStream fos = new FileOutputStream(file);
         fos.write(getData);
-        if(fos!=null){
+        if (fos != null) {
             fos.close();
         }
-        if(inputStream!=null){
+        if (inputStream != null) {
             inputStream.close();
         }
 
 
-        System.out.println("info:"+url+" download success");
+        System.out.println("info:" + url + " download success");
 
     }
 
 
-
     /**
      * 从输入流中获取字节数组
+     *
      * @param inputStream
      * @return
      * @throws IOException
      */
-    public static  byte[] readInputStream(InputStream inputStream) throws IOException {
+    public static byte[] readInputStream(InputStream inputStream) throws IOException {
         byte[] buffer = new byte[1024];
         int len = 0;
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        while((len = inputStream.read(buffer)) != -1) {
+        while ((len = inputStream.read(buffer)) != -1) {
             bos.write(buffer, 0, len);
         }
         bos.close();
@@ -199,8 +205,8 @@ public class HttpRequestUtils {
             conn.setRequestProperty("connection", "Keep-Alive");
             conn.setRequestProperty("user-agent",
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            conn.setConnectTimeout(1000*5);
-            conn.setReadTimeout(1000*5);
+            conn.setConnectTimeout(1000 * 5);
+            conn.setReadTimeout(1000 * 5);
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
@@ -211,7 +217,7 @@ public class HttpRequestUtils {
             // flush输出流的缓冲
             out.flush();
             // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
             String line;
             while ((line = in.readLine()) != null) {
                 result += line;
@@ -234,6 +240,24 @@ public class HttpRequestUtils {
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+//        Map<String, String> header = new HashMap<>();
+//        header.put("token", "6C5D0D5040DE9D97346C7DE844BA6F4D7CB9452F942FAB5A517F699609A3CCA0A5FE8751552B8360EBAF5CBD887023DC24F6E0B18EE5277EDC264CC372C66E8F5D2065F41297A758F391CA5804F234735C6DC5E47C881BE4E388971E5A99FAA8AA09D88E47265A9B79AED5E2C69D8DBF9E18DDFB5DE068C935FA124048119552000FFBA3DE48B1FDF897EA0699502D1E9FD2F6F395BA42F248D4418D7E1833508E4E2E5E678836B3");
+//        try {
+//            String jh = URLEncoder.encode("自7-29-1", "UTF-8");
+//            String s = HttpRequestUtils.sendGet("http://170ef2fa.nat123.fun:10694/DasService/DataService/dgytjtgc/a2/dba05", "ny=202209&jh="+jh, header);
+//            System.out.println(s);
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException(e);
+//        }
+            String s = "";
+        try {
+            System.out.println(                JacksonUtils.jsonToMap(HttpRequestUtils.sendGet("http://170ef2fa.nat123.fun:10694/SSOService/checkAppKey", "appKey=zyqscglpt&appSecret=zyqscglpt@123", null)).get("token"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
